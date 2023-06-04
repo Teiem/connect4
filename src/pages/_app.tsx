40d2cc62bec5d8c4b11d5dc2ae9c083743f9c583
Tim <tim@londschien.com>
2023-06-05 00:45:17 +0200

    change font, use next font loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito as Font } from 'next/font/google';
+
+const font = Font({
+    subsets: ['latin'],
+    weight: "400",
+    style: 'normal',
+    display: 'swap',
+    preload: true,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+          <style jsx global>{`
+            html {
+              font-family: ${font.style.fontFamily};
+            }
+          `}</style>
+          <Component {...pageProps} />
+        </>
+      );
 }
